ec46315efe2f893ce5e885c58b54c4fd9d55d5f0
sdimazana <sdimazana@gmail.com>
2024-08-27 12:44:33 -0700

    Create function that gets player response
    Function getHumanChoice() will prompt the user
    to enter either rock, paper, or scissors. It turns
    their response into a string of all uppercase
    characters so that responses like "Rock", "rock",
    and "rOcK" will all be turned into "ROCK". If the
    player response is either "ROCK", "PAPER", or
    "SCISSORS", the function will log the player's
    choice back to them on the console and return
    either integer 1 for rock, integer 2 for paper,
    and integer 3 for scissors. If the player responds
    with something other than rock, paper, or scissors,
    it will continue to prompt the user till a valid
    response is received.

--- script.js
+++ script.js
@@ -29,3 +29,48 @@ function getComputerChoice(){
         return "scissors";
     }
 }
+
+/*
+    getHumanChoice() algorithm
+
+    Prompt the user to enter either rock, paper, or scissors.
+    Display user choice if they entered a valid response.
+    If invalid, reprompt the user to enter rock, paper, or scissors
+*/
+
+/*
+    getHumanChoice() pseudocode
+
+    Prompt user to enter rock, paper or scissors
+    Turn string to all uppercase characters
+    If user entered something invalid, loop prompt function until valid response
+    If user input equals ROCK
+        Log "You chose rock" to the console and return integer 1.
+    If user input equals PAPER
+        Log "You chose paper" to the console and return integer 2.
+    If user input equals SCISSORS
+        Log "You chose scissors" to the console and return integer 3.
+*/
+
+function getHumanChoice(){
+
+    let humanChoice = prompt("Please enter rock, paper, or scissors: ", "");
+    humanChoice = humanChoice.toUpperCase();
+
+    while (humanChoice !== "ROCK" && humanChoice !== "PAPER" && humanChoice !== "SCISSORS"){
+        humanChoice = prompt("Please enter rock, paper, or scissors: ", "");
+        humanChoice = humanChoice.toUpperCase();
+    }
+
+    switch(humanChoice){
+        case "ROCK": 
+            console.log("You chose ROCK");
+            return 1;
+        case "PAPER": 
+            console.log("You chose PAPER");
+            return 2;
+        case "SCISSORS": 
+            console.log("You chose SCISSORS");
+            return 3;
+    }
+}
