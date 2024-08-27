3315937e1a5746c8457261c862d78c61ff69c439
sdimazana <sdimazana@gmail.com>
2024-08-27 11:42:00 -0700

    Create function that acts as computer opponent
    The goal of this function is for it to randomly
    return one of three sting values: "rock", "paper",
    or "scissors". A random number is generated between
    0 and 0.99. If the number is less than 0.33, the
    function returns "rock". If greater than or equal
    to 0.33 and less than 0.66, it returns "paper". If
    greater than or equal to 0.67, it returns
    "scissors".

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('Hello!');
+/*
+    getComputerChoice() algorithm
+
+    Generate a random number between 0 and 1
+    Test whether number is less than 0.33 
+        If so, return "rock"
+    Test whether number is greater than or equal to 0.33 and less than 0.66
+        If so, return "paper"
+    Test whether number is greater than or equal to 0.67
+        If so, return "scissors"
+*/
+
+/*
+    getComputerChoice() pseudocode
+
+    Declare randomNumber variable and set it equal to Math.random()
+    If randomNumber is less than 0.33, return "rock" ELSE
+    If randomNumber is less than 0.66, return "paper" ELSE
+    Return "scissors"
+*/
+
+function getComputerChoice(){
+    let randomNumber = Math.random();
+    if (randomNumber < 0.33){
+        return "rock";
+    } else if (randomNumber < 0.66){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
