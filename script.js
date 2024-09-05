8aff0f61907fee352ec5e0c5c01a02731a00d2aa
sdimazana <sdimazana@gmail.com>
2024-09-05 12:39:58 -0700

    Complete user interface

--- script.js
+++ script.js
@@ -31,52 +31,6 @@ function getComputerChoice(){
 }
 
 /*
-    getHumanChoice() algorithm
-
-    Prompt the user to enter either rock, paper, or scissors.
-    Display user choice if they entered a valid response.
-    If invalid, reprompt the user to enter rock, paper, or scissors
-*/
-
-/*
-    getHumanChoice() pseudocode
-
-    Prompt user to enter rock, paper or scissors
-    Store response in humanChoice variable
-    Turn humanChoice to all uppercase characters
-    If user entered something invalid, loop prompt function until valid response
-    If humanChoice equals ROCK
-        Log "You chose rock" to the console and return humanChoice
-    If humanChoice equals PAPER
-        Log "You chose paper" to the console and return humanChoice
-    If humanChoice equals SCISSORS
-        Log "You chose scissors" to the console and return humanChoice
-*/
-
-function getHumanChoice(){
-
-    let humanChoice = prompt("Please enter rock, paper, or scissors: ", "");
-    humanChoice = humanChoice.toUpperCase();
-
-    while (humanChoice !== "ROCK" && humanChoice !== "PAPER" && humanChoice !== "SCISSORS"){
-        humanChoice = prompt("Please enter rock, paper, or scissors: ", "");
-        humanChoice = humanChoice.toUpperCase();
-    }
-
-    switch(humanChoice){
-        case "ROCK": 
-            console.log("You chose ROCK");
-            return humanChoice;
-        case "PAPER": 
-            console.log("You chose PAPER");
-            return humanChoice;
-        case "SCISSORS": 
-            console.log("You chose SCISSORS");
-            return humanChoice;
-    }
-}
-
-/*
     playRound() algorithm
 
     Evaluate the player's choice:
@@ -102,87 +56,7 @@ function getHumanChoice(){
     Display tie announcement if it is a tie.
 */
 
-
-/*
-    playGame() algorithm
-
-    This function will keep track of the number of rounds that have been played.
-    After five rounds, the winner is decided.
-    If the player won at least 3 rounds, they are the winner.
-    If the computer won at least 3 rounds, they are the winner.
-    If there were 5 consecutive ties, there is an overall tie.
-*/
-
-/*
-    playGame() pseudocode
-
-    Declare roundCounter which increments each time playRound() is called.
-    While roundCounter is less than or equal to 5, call playRound().
-    If humanScore and computerScore are equal, display tie announcement.
-    If humanScore is greater than computerScore, display winner announcement.
-    If humanScore is less than computerScore, display loser announcement.
-*/
-
-function playGame() {
-
-    //These variables keep track of the scores
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(player, computer){
-        if(computer === player){
-            humanScore++;
-            computerScore++;
-            console.log(`Tie! The computer also chose ${computer}`);
-        return;
-        }
-        switch(player){
-            case "ROCK": 
-                if (computer == "SCISSORS") {
-                    humanScore++;
-                    console.log(`You win! ${player} beats ${computer}`);
-                } else {
-                    computerScore++;
-                    console.log(`You lose! ${computer} beats ${player}`);
-                }
-            break;
-            case "PAPER": 
-                if (computer == "ROCK") {
-                    humanScore++;
-                    console.log(`You win! ${player} beats ${computer}`);
-                } else {
-                    computerScore++;
-                    console.log(`You lose! ${computer} beats ${player}`);
-                }
-            break;
-            case "SCISSORS": 
-                if (computer == "PAPER") {
-                    humanScore++;
-                    console.log(`You win! ${player} beats ${computer}`);
-                } else {
-                    computerScore++;
-                    console.log(`You lose! ${computer} beats ${player}`);
-                }
-            break;
-        }
-    }
-   
-    // let roundCounter = 0;
-
-    // while (roundCounter < 5) {
-    //     roundCounter++;
-    //     playRound(getHumanChoice(), getComputerChoice());
-    // }
-    
-    // if (humanScore === computerScore) {
-    //     console.log("5 rounds are over. Game is a tie!");
-    // } else if (humanScore < computerScore){
-    //     console.log("5 rounds are over. You have lost.");
-    // } else {
-    //     console.log("5 rounds are over. You have won!");
-    // }
-}
-
+//Play a round each time the player clicks a button
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -190,33 +64,82 @@ buttons.forEach((button) => {
     });
 });
 
+//These variables keep track of the scores
+let humanScore = 0;
+let computerScore = 0;
+
 function playRound(player, computer){
+
+    let announcement = "";
+
     if(computer === player){
-        console.log(`Tie! The computer also chose ${computer}`);
-    return;
+        humanScore++;
+        computerScore++;
+        announcement = `Tie! The computer also chose ${computer}`;
+        countScores();
+        displayScores(announcement);
+        return;
     }
+
     switch(player){
         case "ROCK": 
             if (computer == "SCISSORS") {
-                console.log(`You win! ${player} beats ${computer}`);
+                humanScore++;
+                announcement = `You win! ${player} beats ${computer}`;
             } else {
-                console.log(`You lose! ${computer} beats ${player}`);
+                computerScore++;
+                announcement = `You lose! ${computer} beats ${player}`;
             }
         break;
         case "PAPER": 
             if (computer == "ROCK") {
-                console.log(`You win! ${player} beats ${computer}`);
+                humanScore++;
+                announcement = `You win! ${player} beats ${computer}`;
             } else {
-                console.log(`You lose! ${computer} beats ${player}`);
+                computerScore++;
+                announcement = `You lose! ${computer} beats ${player}`;
             }
         break;
         case "SCISSORS": 
             if (computer == "PAPER") {
-                console.log(`You win! ${player} beats ${computer}`);
+                humanScore++;
+                announcement = `You win! ${player} beats ${computer}`;
             } else {
-                console.log(`You lose! ${computer} beats ${player}`);
+                computerScore++;
+                announcement = `You lose! ${computer} beats ${player}`;
             }
         break;
     }
+
+    displayScores(announcement);
+    countScores();
 }
 
+function displayScores(announcement){
+    const roundAnnouncement = document.querySelector('#roundAnnoucement');
+    roundAnnouncement.textContent = announcement;
+
+    const computerScoreDisplay = document.querySelector('#computerScore');
+    computerScoreDisplay.textContent = computerScore;
+
+    const playerScoreDisplay = document.querySelector('#playerScore');
+    playerScoreDisplay.textContent = humanScore;
+}
+
+function countScores(){
+    let winner = '';
+    if(humanScore == 5){
+        winner = 'Congratulations! You won!';
+        resetGame();
+    }else if (computerScore == 5){
+        winner = 'The computer has won!';
+        resetGame();
+    }
+    const winnerAnnouncement = document.querySelector('#winnerAnnouncement');
+    winnerAnnouncement.textContent = winner;
+}
+
+function resetGame(){
+    humanScore = 0;
+    computerScore = 0;
+}
